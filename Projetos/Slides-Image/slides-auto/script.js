422dcf398c6bae7cb9e74b3fbf2580c7be577018
Ramon Cibas <ramon.cibas@hotmail.com>
2021-02-03 17:55:24 -0300

    some updates

---
+++ Projetos/Slides-Image/slides-auto/script.js
@@ -0,0 +1,24 @@
+let time = 2000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#slider img"),
+    max = images.length;
+
+function nextImage() {
+    images[currentImageIndex].classList.remove("selected")
+    
+    currentImageIndex++
+    
+    if (currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex].classList.add("selected")
+}
+
+function start () {
+    setInterval(() => {
+        nextImage()
+    },time)    
+}
+
+// inicia a funcao assim que a janela for carregada
+window.addEventListener("load", start)
