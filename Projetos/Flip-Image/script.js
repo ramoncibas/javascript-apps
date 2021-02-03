7a64ed42d8a766892c226b3a031170a2adef3937
Ramon Cibas <ramon.cibas@hotmail.com>
2021-02-02 22:00:04 -0300

    bugs fixed

--- Projetos/Flip-Image/script.js
+++ Projetos/Flip-Image/script.js
@@ -1,24 +1,30 @@
 var slideIndex = 1
 showSlides(slideIndex)
 
+// next/prev controls
 function nextAndPrev(e) {
     showSlides(slideIndex += e)
 }
 
-function showSlides(e) {    
-    let slides = document.getElementsByClassName('slide-image')    
+// thumbnail image controls
+function currentSlide(e) {
+    showSlides(slideIndex = e)
+}
 
-    if (e > slides.length) {
-        slideIndex = 1
-    }
-
-    if (e < slides.length) {
+function showSlides(e) {
+    let slides = document.getElementsByClassName('slide-image')
+    //let largeImg = document.getElementById('large-image')
+    
+    if (e > slides.length)
+        slideIndex = 1    
+    if (e < 1)
         slideIndex = slides.length
-    }
 
-    for (i=0; i < slides.length; i++) {
+    // esconder o ultimo slide
+    for (i=0; i < slides.length; i++) { 
         slides[i].style.display = "none"
     }
 
-    slides[slideIndex-1].style.display = "block"    
+    // mostra o slide respectivo
+    slides[slideIndex-1].style.display = "block"   
 }
