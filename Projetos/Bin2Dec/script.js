33c098796423fb8cc4e269e06b07df757a23752a
Ramon Cibas <ramon.cibas@hotmail.com>
2021-02-08 17:07:29 -0300

    some updates

--- Projetos/Bin2Dec/script.js
+++ Projetos/Bin2Dec/script.js
@@ -3,18 +3,20 @@ function bin2dec() {
     let txtdec = document.querySelector('input#txtdec')
 
     if (txtbin.value.length > 8) {
-        alert('Máximo de 8 digitos.')
+        alert('Máximo de 8 digitos!')
+
+    } else if (isNaN(txtbin.value)) {
+        alert('Valor digitado invalido!')
 
     } else {
-        let binario = txtbin.value.split('').reverse()            
+        let binario = txtbin.value.split('').reverse()
         let decimal = 0
-      
+
         for (let c = 0; c < binario.length; c++) {
             if (binario[c] === '1') {
                 decimal += Math.pow(2, c)
             }
-        }    
-
+        }
         txtdec.value = decimal
-    }        
+    }
 }
