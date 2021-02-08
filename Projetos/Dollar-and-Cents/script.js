e0e53bc48f1b6468f910456ea5cf7533407675c7
Ramon Cibas <ramon.cibas@hotmail.com>
2021-02-08 17:08:55 -0300

    to dollar

--- Projetos/Dollar-and-Cents/script.js
+++ Projetos/Dollar-and-Cents/script.js
@@ -1,14 +1,14 @@
-function convert() {
-    let inputValues = document.querySelector('input#txtvalue').value    
-    let res = document.querySelector('div#res')    
+function toCents() {
+    let inputValues = document.querySelector('input#txtvalue').value
+    let res = document.querySelector('div#res')
 
     if (inputValues == '') {
-        alert('Digite um valor a ser convertido em ¢!')              
-        
+        alert('Digite um valor a ser convertido em ¢!')
+
     } else if (inputValues > 1) {
         alert('Valor maior que o esperado!')
-    
-    } else {        
+
+    } else {
         function coins(number) {
             // Quarters 25 ¢, Dimes 10 ¢, Nickels 5 ¢, Pennies 1 ¢
             let penny, nickel, dime, quarter, res;
@@ -22,7 +22,7 @@ function convert() {
             res = res % 5
 
             penny = Math.floor(res / 1)
-            
+
             return `
                 <p class="p_output"> Cents:
                     <br>Quarter <strong>(¢25)</strong> = ${quarter}
@@ -32,10 +32,57 @@ function convert() {
                 </p>
             `
         }
-        
+
         let cents;
         // Convertendo dolars para centavos. Math.round() é usado para arredondar numeros 0.55 - 0.58
         cents = Math.round(inputValues * 100)
-        res.innerHTML = `<p class="p_output">Total de cents: ${cents}</p>` + coins(cents)        
-    }    
+        res.innerHTML = `<p class="p_output">Total de cents: ${cents}</p>` + coins(cents)
+    }
+}
+
+function toDolar() {
+    let inputValues = document.querySelector('input#txtvalue').value
+    let res = document.querySelector('div#res')
+
+    if (inputValues == '') {
+        alert('Digite um valor a ser convertido em U$!')
+    } else {
+        function dolls(number) {
+            let washington, tJefferson, lincoln, hamilton, jackson, grant, bFranklin;
+
+            bFranklin = Math.floor(number / 100)
+            res = number % 100
+
+            grant = Math.floor(res / 50)
+            res = res % 50
+
+            jackson = Math.floor(res / 20)
+            res = res % 20
+
+            hamilton = Math.floor(res / 10)
+            res = res % 10
+
+            lincoln = Math.floor(res / 5)
+            res = res % 5
+
+            tJefferson = Math.floor(res / 2)
+            res = res % 2
+
+            washington = Math.floor(res / 1)
+
+            return `
+                <p class="p_output"> President Dolls:
+                <br>P. Benjami F. Dolls <strong>(U$100)</strong> = ${bFranklin}
+                <br>P. Grant Dolls <strong>(U$50)</strong> = ${grant}
+                <br>P. Jackson Dolls <strong>(U$20)</strong>= ${jackson}
+                <br>P. Hamilton Dolls <strong>(U$10)</strong> = ${hamilton}
+                <br>P. A. Lincoln Dolls <strong>(U$5)</strong> = ${lincoln}
+                <br>P. Thomas J. Dolls <strong>(U$2)</strong> = ${tJefferson}
+                <br>P. Washington Dolls <strong>(U$1)</strong> = ${washington}
+                </p>
+            `
+        }
+
+        res.innerHTML = `<p class="p_output">Total de cents: ${inputValues}</p>` + dolls(inputValues)
+    }
 }
