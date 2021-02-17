c1f37a0f26b06fe689f3897ba37c5b602b75560c
Ramon Cibas <ramon.cibas@hotmail.com>
2021-02-17 20:32:13 -0300

    some updates

--- Projetos/Notes/script.js
+++ Projetos/Notes/script.js
@@ -1,12 +1,29 @@
-const add_btn = document.querySelector("button .btn-add")
-const close_btn = document.querySelector("button .btn-close")
-var content = document.getElementById("container")
-
+// abrindo o pop-up para adicionar a nota
 function addNote() {
     let popup = document.getElementById("pop-up-bg")
-    popup.style.display = 'flex'
+    let title_popup = document.getElementById("txtpopup")
+    let txt_popup = document.getElementById("txtnote")
 
+    popup.style.display = 'flex'   
+    
+    if (title_popup != "" || txt_popup != "") {
+        title_popup.value = ''
+        txt_popup.value = ''        
+    }
+}
 
+// salvando a nota
+function saveNote() {
+    let title_popup = document.getElementById("txtpopup").value
+    let txt_popup = document.getElementById("txtnote").value
+    
+    createNote(title_popup, txt_popup)        
+    closePopUp()
+}
+
+// criando elemento dentro do campo notas
+function createNote(title, text) {
+    let content = document.getElementById("container")
     let div = document.createElement("div")
     let label = document.createElement("label")
     let input = document.createElement("input")
@@ -14,17 +31,15 @@ function addNote() {
     div.setAttribute("class", "note")
     content.appendChild(div)
 
-    label.textContent = "Acordadar cedo"
+    label.textContent = title
     div.appendChild(label)
 
-    input.value = "Levantar 6hr da manhã"
+    input.value = text
+    input.readOnly = true
     div.appendChild(input)
 }
 
-function saveNote() {
-    closePopUp()
-}
-
+// fehando popup
 function closePopUp() {
     let popup = document.getElementById("pop-up-bg")
     popup.style.display = 'none'
