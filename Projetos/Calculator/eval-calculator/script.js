9067c69c06ee71e8cc8f47788998b18eb8e332d0
Ramon Cibas <ramon.cibas@hotmail.com>
2021-03-09 16:34:13 -0300

    add show historic

--- Projetos/Calculator/eval-calculator/script.js
+++ Projetos/Calculator/eval-calculator/script.js
@@ -1,20 +1,15 @@
 // Adicionando o numero clicado ao input contendo seu respectivo valor
-function addValue(elementId) { 
-    let getNumber = document.getElementById("putValues")
-    
+function addValue(elementId) {     
     // Tratando possiveis erros
     if (getNumber.value == "0" || getNumber.value == "Syntax error") { 
         getNumber.value = elementId
     } else {
         getNumber.value += elementId
-        console.log(elementId)
-    }
+        console.log(elementId)       
 }
 
 // Adicionando o operador clicado ao input contendo seu respectivo valor
-function addOperator(elementId) {
-    let getNumber = document.getElementById("putValues")
-    
+function addOperator(elementId) {    
     // Tratando possiveis erros
     if (getNumber.value == "0" || getNumber.value == "Syntax error") { 
         getNumber.value = "0"
@@ -25,18 +20,14 @@ function addOperator(elementId) {
 }
 
 // Incluindo valores decimais
-function setDecimal(elementId, status) { 
-    let getNumber = document.getElementById("putValues")
-    
+function setDecimal(elementId, status) {     
     getNumber.value += elementId.textContent
     //document.getElementById("dot").disable = status
     console.log(elementId.textContent)
 }
 
 // Removendo o ultimo valor digitado dentro do input
-function removeLastNumber() { 
-    let getNumber = document.getElementById("putValues")
-    
+function removeLastNumber() {     
     if (getNumber.value.length == 1 || getNumber.value == "0") {
         getNumber.value = "0"
         document.getElementById("dot").disable = false
@@ -55,13 +46,38 @@ function clearAll() {
 // Calculando o resultado com os respectivos valores dentro do input
 function calculateResult() { 
     try {
-        let getNumber = document.getElementById("putValues");
         if (getNumber.value != "") {
             let calculateResult = eval(getNumber.value);
             getNumber.value = calculateResult;
-            console.log(getNumber.value)
+            console.log(getNumber.value)                        
         }
     } catch (err) {    
-        getNumber.value = "Syntax error";    
+        getNumber.value = "Syntax error";
     }    
+}
+
+function showHistoric() {
+    let calc_buttons = document.querySelector("#calculator-buttons")
+    let show_historic = document.querySelector("#showHistoric")
+    let input_historic = document.querySelector("#inputHistoric")
+    let elementsHistoric = document.querySelectorAll(".inputRes")
+
+    calc_buttons.classList.add("sectionUnder")
+    show_historic.classList.add("historic")
+
+    for (e of elementsHistoric) {
+        e.remove()
+    }
+
+
+
+}
+
+// Função que fecha todo o historico e volta a calculadora
+function closeHistoric() {
+    let calc_buttons = document.querySelector("#calculator-buttons")
+    let historic = document.querySelector("#showHistoric")
+    
+    calc_buttons.classList.remove("sectionUnder")
+    historic.classList.remove("historic")
 }
