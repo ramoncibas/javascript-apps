f7db0e818810b32d64478956ea8a47adbb59e922
Ramon Cibas <ramon.cibas@hotmail.com>
2021-03-09 17:42:47 -0300

    new app

---
+++ Projetos/Weather-App/script.js
@@ -0,0 +1,35 @@
+let appId; //apikey
+let units = "imperial";
+let searchMethod = "zip";
+
+function getSearchMethod(searchTerm) {
+    // Validações
+    if(searchTerm.length === 5) {
+        // Zip - Codigo postal
+        searchMethod = "zip"
+    } else {
+        // Querry - Nome da cidade
+        searchMethod = "q"
+    }
+}
+
+function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm)
+    // Pegando a informação vinda do servidor
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result =>{
+        // Convertendo json
+        return result.json()
+    }).then(result => {
+        console.log(result)
+    })    
+}
+
+// Passando a funão para o botão
+document.getElementById("searchBtn").addEventListener("click", () => {
+    let searchTerm = document.getElementById("searchInput").value
+    
+    // Verificando se há valor
+    if(searchTerm) {
+        searchWeather(searchTerm)
+    }
+})
