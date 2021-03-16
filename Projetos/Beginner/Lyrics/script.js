6907d4e0d9758faa8874786b1ce1679d15ba9ec1
Ramon Cibas <ramon.cibas@hotmail.com>
2021-03-16 17:59:25 -0300

    some update

--- Projetos/Beginner/Lyrics/script.js
+++ Projetos/Beginner/Lyrics/script.js
@@ -10,18 +10,23 @@ form.addEventListener("submit", el => {
     newSubmit()
 });
 
+// Passando os parametros que o usuario digitar para a busca na "api"
 async function newSubmit() {
     const lyrics = document.querySelector("#lyrics")
     const artist = document.querySelector("#artist")
-    const song = document.querySelector("#song")    
+    const song = document.querySelector("#song")
+
+    // "Buscando..." enquanto não retorna nada da "api"
+    lyrics.innerHTML = '<span class="loading">Buscando...</span>';
 
     try {
         // Pegando a letra da api
         const lyricsRes = await findLyrics(artist.value, song.value)
+        console.log(lyricsRes)
 
         // Retornando um objeto json
-        const data = await lyricsRes.json()        
-        
+        const data = await lyricsRes.json()
+        console.log(data)
         lyrics.innerHTML = data.lyrics;
 
     } catch (err) {
