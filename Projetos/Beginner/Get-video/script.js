3fdb73bdf074215ffca5f699585ea604cf207fb1
Ramon Cibas <ramon.cibas@hotmail.com>
2021-04-07 16:55:08 -0300

    Update script.js

--- Projetos/Beginner/Get-video/script.js
+++ Projetos/Beginner/Get-video/script.js
@@ -10,6 +10,40 @@ function startVideo() {
 
 startVideo()
 
-document.querySelector(".seeAllPicture").addEventListener("click", (event) => {
+document.querySelector(".seeAllPicture").addEventListener("click", () => {
+    let icon = document.querySelector(".seeAllPicture i")
+    let buttonText = document.querySelector(".seeAllPicture p")
+
     document.querySelector(".galery").classList.toggle("fullsize");
+    if (buttonText.textContent == "Galery") {
+        icon.classList.replace("fa-arrow-circle-right", "fa-arrow-circle-left");        
+        buttonText.textContent = "Back";        
+
+    } else if(buttonText.textContent == "Back") {
+        icon.classList.replace("fa-arrow-circle-left", "fa-arrow-circle-right");        
+        buttonText.textContent = "Galery";        
+    }
 });
+
+document.querySelector(".favorite").addEventListener("click", () => {
+    let favoriteIcon = document.querySelector(".favorite i")
+    
+    favoriteIcon.classList.replace("far", "fas") || favoriteIcon.classList.replace("fas", "far")
+});
+
+let pictures = document.querySelectorAll(".picture")
+for(i of pictures) {
+    i.addEventListener("click", (e) => {
+        let picture = e.target;          
+
+        pictures.forEach(removeLargeClass)
+        
+        function removeLargeClass(picture) {
+            picture.classList.remove("large")            
+        }
+
+        picture.classList.add("large")
+        document.querySelector(".galery").classList.add("width");
+        document.querySelector(".scroll-pictures").classList.add("width");
+    });
+}
