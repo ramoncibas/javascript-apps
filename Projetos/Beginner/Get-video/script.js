4bbf80cc29a79d0983306c3374242df104bee2ff
Ramon Cibas <ramon.cibas@hotmail.com>
2021-04-09 16:35:28 -0300

    Update script.js

--- Projetos/Beginner/Get-video/script.js
+++ Projetos/Beginner/Get-video/script.js
@@ -1,45 +1,80 @@
-const video = document.getElementById("video")
+const captureVideoButton = document.querySelector("#screenshot .capture-video");
+const disableVideoButton = document.querySelector("#screenshot .disable-video");
+const screenshotButton = document.querySelector("#screenshot .take-picture");
 
-function startVideo() {
-    navigator.getUserMedia(
-        { video: {} },
-        stream => video.srcObject = stream,
-        err => console.log(err)
-    )
-}
+const video = document.querySelector("#screenshot video");
 
-startVideo()
+captureVideoButton.addEventListener("click", function () {
+    navigator.mediaDevices.getUserMedia({video: true})
+        .then(stream => {
+            screenshotButton.disabled = false;
+            video.srcObject = stream;
+        }).catch(error => {
+            console.error("Error: ", error);
+
+        });
+});
+
+disableVideoButton.addEventListener("click", function () {
+    // O objeto MediaStream de um video está disponivel por meio de um atributo srcObject
+    const mediaStream = video.srcObject;
+
+    // Pegando MediaStreamTracks com getTracks()    
+    const tracks = mediaStream.getTracks();
+    console.log(tracks);
+
+    // Parando todos os processos de tracks
+    tracks.forEach(track => track.stop());
+
+    // Tracks retorna um array, então se quiser para somente um, faça isso
+    //tracks[0].stop();
+
+    // Só é possivel chamar um getUserMedia no projeto, nessa caso usamos para ativar a camera
+    // Então não sera possivel chama-lo novamente para pausa-la
+});
+
+screenshotButton.addEventListener("click", function () {
+    const canvas = document.createElement("canvas");
+    const img = document.querySelector("#screenshot img");
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext("2d").drawImage(video, 0, 0);
+
+    // Em outros browsers ira falhar a volta do img/pgn
+    img.src = canvas.toDataURL("image/webp");
+});
 
 document.querySelector(".seeAllPicture").addEventListener("click", () => {
-    let icon = document.querySelector(".seeAllPicture i")
-    let buttonText = document.querySelector(".seeAllPicture p")
+    let icon = document.querySelector(".seeAllPicture i");
+    let buttonText = document.querySelector(".seeAllPicture p");
 
     document.querySelector(".galery").classList.toggle("fullsize");
     if (buttonText.textContent == "Galery") {
-        icon.classList.replace("fa-arrow-circle-right", "fa-arrow-circle-left");        
-        buttonText.textContent = "Back";        
+        icon.classList.replace("fa-arrow-circle-right", "fa-arrow-circle-left");
+        buttonText.textContent = "Back";
 
-    } else if(buttonText.textContent == "Back") {
-        icon.classList.replace("fa-arrow-circle-left", "fa-arrow-circle-right");        
-        buttonText.textContent = "Galery";        
+    } else if (buttonText.textContent == "Back") {
+        icon.classList.replace("fa-arrow-circle-left", "fa-arrow-circle-right");
+        buttonText.textContent = "Galery";
     }
 });
 
 document.querySelector(".favorite").addEventListener("click", () => {
-    let favoriteIcon = document.querySelector(".favorite i")
-    
-    favoriteIcon.classList.replace("far", "fas") || favoriteIcon.classList.replace("fas", "far")
+    let favoriteIcon = document.querySelector(".favorite i");
+
+    favoriteIcon.classList.replace("far", "fas") || favoriteIcon.classList.replace("fas", "far");
 });
 
-let pictures = document.querySelectorAll(".picture")
-for(i of pictures) {
+let pictures = document.querySelectorAll(".picture");
+for (i of pictures) {
     i.addEventListener("click", (e) => {
-        let picture = e.target;          
+        let picture = e.target;
 
-        pictures.forEach(removeLargeClass)
-        
+        pictures.forEach(removeLargeClass);
+
         function removeLargeClass(picture) {
-            picture.classList.remove("large")            
+            picture.classList.remove("large")
         }
 
         picture.classList.add("large")
