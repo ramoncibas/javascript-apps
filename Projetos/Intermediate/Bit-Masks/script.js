3fbf8d01337c6c71cc83521f5ce2eb78f8b6d6c3
Ramon Cibas <ramon.cibas@hotmail.com>
2021-05-25 18:15:02 -0300

    Update script.js

--- Projetos/Intermediate/Bit-Masks/script.js
+++ Projetos/Intermediate/Bit-Masks/script.js
@@ -2,10 +2,18 @@
 function searchCities() {
     let searchvalue = document.querySelector("input[type=text]");
     let checkbox = document.querySelectorAll("input[type=checkbox]");
-
+    
     for (i = 0; i < checkbox.length; i++) {
         if (checkbox[i].value == searchvalue.value) {
             console.log(checkbox[i]);
         }
     }
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    // atualizando o relogio a cada 1seg
+    setInterval (() => {
+        var hora = new Date().toLocaleTimeString('pt-br');
+        document.querySelector(".clock").innerHTML = hora;
+    }, 1000)
+})
