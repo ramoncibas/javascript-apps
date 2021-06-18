375f6c23f61c9c49df9df72b5e4f64cd98604150
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-18 18:06:09 -0300

    Update script.js

--- Projetos/Intermediate/GitHub-Profiles/script.js
+++ Projetos/Intermediate/GitHub-Profiles/script.js
@@ -1,49 +1,98 @@
-// Elementos de buscar repositorio
-let searchButton = document.querySelector(".search-button");
-let userName = document.querySelector(".search-repos input");
-
-let language = document.querySelectorAll(".repos-info .language")
-
 // Buscar o usuario principal da aplicacao
 async function searchUser(username) {
-  try {
+  return(
     fetch(`http://api.github.com/users/${username}/repos`).then((response) => {
       if (response.ok) {
         response.json().then((data) => {
-          let containerRepos = document.querySelector(".container-repos");
+          let containerRepos = document.querySelector(".container-repos.my");
+          let userProfileName = document.querySelector(".user-info h4");
+          let userProfileImg = document.querySelector(".avatar-user img");
           
-          // Inserindo os repositorios no container
+          // Atribuindo nome e a o avatar do usuario
+          userProfileName.textContent = data[0].owner.login;
+          userProfileImg.src = data[0].owner.avatar_url;
+          
+          // Inserindo os repositorios no container        
           for (i of data) {
-            containerRepos.innerHTML += createElement(
+            containerRepos.innerHTML += createElementsOfMyRepos(
               i.name, i.description, i.language, i.created_at, i.url
             );
           }
           insertingDataIntoLocalStorage(username, true);
-        });        
+        });
 
       } else {
         alert("Ops! Usuario não encontrado...")
         return false;
       }
-    });
+    })
+  );
+}
+
+// Funcao que busca por novos repositorios
+async function searchReposGh(username) {
+  try {
+    fetch(`https://api.github.com/users/${username}/repos`).then(response => {
+      if (response.ok) {
+        response.json().then(data => {
+          let containerRepos = document.querySelector(".container-repos.other");
+          
+          if (!containerRepos.lengh) containerRepos.innerHTML = "";
+          
+          for (i of data) {              
+            containerRepos.innerHTML += createElementsOfOtherRepos(
+              i.owner.avatar_url, i.owner.login, i.name, i.description, i.language, i.created_at, i.url
+            );
+          }
+        })
+      } else {
+        alert("Ops! Usuario não encontrado...")
+        return false;
+      }
+    })
   } catch(error) {
-    console.log(error)    
+    console.log(error);
     alert("Ops! Alguma coisa deu errado!")
   }
 }
 
-// Função que insere nome do usuario e se é a primeira visita no site do usuario no local storage
-function insertingDataIntoLocalStorage(username, firsaccess) {
-  var data = {
-    firstAccess: firsaccess,
-    userName: username
-  }
-  
-  localStorage.setItem("first-access", JSON.stringify(data));  
+// Chamando a função que busca por novos repositorios
+async function searchRepositories() {
+  let inputUserName = document.querySelector(".search-repos input");
+  await searchReposGh(inputUserName.value)
 }
 
-// Função que retorna os cards com informaçoes do repositorio
-function createElement(name, description, language, created_at, url) {  
+// Função que retorna os cards com informaçoes de outros repositorios
+function createElementsOfOtherRepos(avatar, username, name, description, language, created_at, url){
+  return `
+    <div class="user-repos">
+      <div class="repos-content">
+
+        <div class="user-profile">
+          <img src="${avatar}" class="avatar-gh">
+          <h2>${username}</h2>
+        </div>
+
+        <div class="repos-info">
+          <h5 class="repos">Repositório: ${name}</h5>
+          <h5 class="description">Descrição: ${description}</h5>
+          <h5 class="language">Language: ${language}</h5>
+          <h5 class="created">Criado em: ${created_at}</h5>
+        </div>
+
+        <button class="see-repos btn hover-white"
+          onclick="location.href='${url}'"
+        >
+          Ver repositorio
+          <i class="fab fa-github"></i>
+        </button>
+      </div>
+    </div>
+  `;
+}
+
+// Função que retorna os cards com informaçoes do repositorio do "usuario"
+function createElementsOfMyRepos(name, description, language, created_at, url) {
   return `
     <div class="user-repos">  
       <div class="repos-info">
@@ -51,7 +100,7 @@ function createElement(name, description, language, created_at, url) {
         <h5 class="description">Descrição: ${description}</h5>
         <h5 class="language">Linguagem: ${language}</h5>
         <h5 class="created">Criado em: ${created_at}</h5>
-        <button class="see-repos btn hover-white" 
+        <button class="see-repos btn hover-white"
           onclick="location.href='${url}'"
         >
           Ver repositorio
@@ -74,6 +123,16 @@ function modalSearchProfile() {
       </div>
     </div>
   `;
+}
+
+// Função que insere nome do usuario e se é a primeira visita no site do usuario no local storage
+function insertingDataIntoLocalStorage(username, firsaccess) {
+  var data = {
+    firstAccess: firsaccess,
+    userName: username
+  }
+  
+  localStorage.setItem("first-access", JSON.stringify(data));  
 }
 
 window.addEventListener("DOMContentLoaded", () => {
